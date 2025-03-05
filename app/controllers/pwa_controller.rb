0c80b8f9e2f721757226522e066b799a896fe940
Jolyon Pawlyn <jpawlyn@gmail.com>
2025-03-05 19:06:52 +0100

    Switch offline action to pwa controller

--- app/controllers/pwa_controller.rb
+++ app/controllers/pwa_controller.rb
@@ -2,11 +2,15 @@ require "rails/application_controller"
 
 # see https://alicia-paz.medium.com/make-your-rails-app-work-offline-part-2-caching-assets-and-adding-an-offline-fallback-334729ade904
 # and https://github.com/rails/rails/pull/54657
-class PwaController < Rails::ApplicationController
+class PwaController < ActionController::Base
   skip_forgery_protection
   content_security_policy false, only: :service_worker
+  layout "application", only: :offline
+
 
   def manifest; end
 
   def service_worker; end
+
+  def offline; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   get "manifest" => "pwa#manifest", as: :pwa_manifest
   get "service-worker" => "pwa#service_worker", as: :pwa_service_worker
+  get "offline" => "pwa#offline"
 
   # Defines the root path route ("/")
   root "home#index"
 
   get "show" => "home#show"
-  get "offline" => "home#offline"
 end
